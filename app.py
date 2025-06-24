27b097dc3c4305e598069039a27af9c2ebd102a4
heiwanasora <heiwanasora@gmail.com>
2025-06-24 19:24:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})  # ← CORS全許可
 
 @app.route("/")
 def home():
