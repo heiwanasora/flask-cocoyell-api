6452c773886fda4e5b9b7c5009eb366d129529fb
heiwanasora <heiwanasora@gmail.com>
2025-07-03 11:56:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -84,5 +84,5 @@ def message():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=False, host="0.0.0.0", port=10000)
+    app.run(debug=False, host="0.0.0.0", port=600)
 
