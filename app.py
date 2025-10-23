f1abdac7eab1b2750f419928116cd9ca7c6ac906
heiwanasora <heiwanasora@gmail.com>
2025-10-23 17:38:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -144,5 +144,5 @@ def root():
     return "✅ Smith LINE Analyzer — ❤️ハート5段階＋スコア＋UTF-8対応", 200
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5001))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
